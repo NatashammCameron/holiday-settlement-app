6301e7c3c36c138983a18d126b26f4ecfb7308a5
NatashammCameron <cameronnatasha12@gmail.com>
2026-08-07 16:09:57 +0100

    Implement holiday creation endpoint

--- backend/app/routes/holiday_routes.py
+++ backend/app/routes/holiday_routes.py
@@ -1,4 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from sqlalchemy.orm import Session
+
+from app.database.dependencies import get_db
+from app.models.holiday import Holiday
+from app.schemas.holiday_schema import (
+    HolidayCreate,
+    HolidayResponse
+)
 
 router = APIRouter(
     prefix="/holidays",
@@ -6,8 +15,20 @@ router = APIRouter(
 )
 
 
-@router.get("/")
-def get_holidays():
-    return {
-        "message": "Holiday router working"
-    }
+@router.post(
+    "/",
+    response_model=HolidayResponse
+)
+def create_holiday(
+    holiday: HolidayCreate,
+    db: Session = Depends(get_db)
+):
+    new_holiday = Holiday(
+        name=holiday.name
+    )
+
+    db.add(new_holiday)
+    db.commit()
+    db.refresh(new_holiday)
+
+    return new_holiday
