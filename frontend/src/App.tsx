fc12aac3e34252f4a5b71b61d269d83025c4c612
NatashammCameron <cameronnatasha12@gmail.com>
2026-08-08 15:03:02 +0100

    Create React frontend foundation

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function Dashboard() {
+    return (
+        <div>
+            <h1>Holiday Settlement App</h1>
+            <h2>Dashboard</h2>
+        </div>
+    );
+}
+
+export default Dashboard;
